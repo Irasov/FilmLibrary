6fd193672b2caa6949d7841eea052af317eb94e6
Irasov <srnan@yandex.ru>
2015-07-25 16:04:45 +0600

    System USer

---
+++ src/main/java/com/epam/irasov/filmlibrary/entity/SystemUser.java
@@ -0,0 +1,62 @@
+package com.epam.irasov.filmlibrary.entity;
+
+import java.time.LocalDate;
+
+public abstract class SystemUser extends Member{
+    private String login;
+    private String password;
+    private String email;
+
+    public SystemUser() {
+
+    }
+
+    public SystemUser(Long id, String name, String patronymic, String surname, LocalDate birthDate, String photo, Type type, String login, String password, String email) {
+        super(id, name, patronymic, surname, birthDate, photo, type);
+        this.login = login;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SystemUser)) return false;
+        if (!super.equals(o)) return false;
+        SystemUser that = (SystemUser) o;
+        return !(login != null ? !login.equals(that.login) : that.login != null) && !(password != null ? !password.equals(that.password) : that.password != null) && !(email != null ? !email.equals(that.email) : that.email != null);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (login != null ? login.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+}
