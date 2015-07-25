275dbb30a0f63d217c8e0fce97ded6c3e56cfe9a
Irasov <srnan@yandex.ru>
2015-07-25 15:24:20 +0600

    List <Honor >honors

--- src/main/java/com/epam/irasov/filmlibrary/entity/Film.java
+++ src/main/java/com/epam/irasov/filmlibrary/entity/Film.java
@@ -15,12 +15,14 @@ public class Film extends NamedEntity {
     private String description;
     private List<Review> reviews;
     private Rating rating;
+    private List<Honor> honors;
 
     public Film() {
         super();
         members = new ArrayList<>();
         genres = new ArrayList<>();
         reviews = new ArrayList<>();
+        honors = new ArrayList<>();
     }
 
 
@@ -53,6 +55,18 @@ public class Film extends NamedEntity {
 
     public void addMember(Member member){
         members.add(member);
+    }
+
+    public List<Honor> getHonors() {
+        return honors;
+    }
+
+    public void setHonors(List<Honor> honors) {
+        this.honors = honors;
+    }
+
+    public void addHonors(Honor honor){
+        honors.add(honor);
     }
 
     public List<Genre> getGenres() {
@@ -133,7 +147,7 @@ public class Film extends NamedEntity {
         if (!(o instanceof Film)) return false;
         if (!super.equals(o)) return false;
         Film film = (Film) o;
-        return ageRestriction == film.ageRestriction && duration == film.duration && !(tagLine != null ? !tagLine.equals(film.tagLine) : film.tagLine != null) && !(members != null ? !members.equals(film.members) : film.members != null) && !(genres != null ? !genres.equals(film.genres) : film.genres != null) && !(premiere != null ? !premiere.equals(film.premiere) : film.premiere != null) && !(cover != null ? !cover.equals(film.cover) : film.cover != null) && !(description != null ? !description.equals(film.description) : film.description != null) && !(reviews != null ? !reviews.equals(film.reviews) : film.reviews != null) && !(rating != null ? !rating.equals(film.rating) : film.rating != null);
+        return ageRestriction == film.ageRestriction && duration == film.duration && !(tagLine != null ? !tagLine.equals(film.tagLine) : film.tagLine != null) && !(members != null ? !members.equals(film.members) : film.members != null) && !(genres != null ? !genres.equals(film.genres) : film.genres != null) && !(premiere != null ? !premiere.equals(film.premiere) : film.premiere != null) && !(cover != null ? !cover.equals(film.cover) : film.cover != null) && !(description != null ? !description.equals(film.description) : film.description != null) && !(reviews != null ? !reviews.equals(film.reviews) : film.reviews != null) && !(rating != null ? !rating.equals(film.rating) : film.rating != null) && !(honors != null ? !honors.equals(film.honors) : film.honors != null);
 
     }
 
@@ -150,6 +164,7 @@ public class Film extends NamedEntity {
         result = 31 * result + (description != null ? description.hashCode() : 0);
         result = 31 * result + (reviews != null ? reviews.hashCode() : 0);
         result = 31 * result + (rating != null ? rating.hashCode() : 0);
+        result = 31 * result + (honors != null ? honors.hashCode() : 0);
         return result;
     }
 }
