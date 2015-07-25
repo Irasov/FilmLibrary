57bf728e9e5f99d54e16ec27c16164d13be64a53
Irasov <srnan@yandex.ru>
2015-07-25 15:30:40 +0600

    FilmMember

---
+++ src/main/java/com/epam/irasov/filmlibrary/entity/FilmMember.java
@@ -0,0 +1,46 @@
+package com.epam.irasov.filmlibrary.entity;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilmMember extends Member {
+    private List<Honor> honors;
+
+    public FilmMember() {
+    }
+
+    public FilmMember(Long id, String name, String patronymic, String surname, LocalDate birthDate, String photo, Type type, List<Honor> honors) {
+        super(id, name, patronymic, surname, birthDate, photo, type);
+        honors = new ArrayList<>();
+    }
+
+    public List<Honor> getHonors() {
+        return honors;
+    }
+
+    public void setHonors(List<Honor> honors) {
+        this.honors = honors;
+    }
+
+    public void addHonor(Honor honor){
+        honors.add(honor);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FilmMember)) return false;
+        if (!super.equals(o)) return false;
+        FilmMember that = (FilmMember) o;
+        return !(honors != null ? !honors.equals(that.honors) : that.honors != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (honors != null ? honors.hashCode() : 0);
+        return result;
+    }
+}
