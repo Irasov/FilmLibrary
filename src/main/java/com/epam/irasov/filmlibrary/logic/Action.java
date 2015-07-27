6e07a45d08ba1b2331dec07a3c6aa8d4ca559a04
Irasov <srnan@yandex.ru>
2015-07-27 13:20:53 +0600

    countRating

--- src/main/java/com/epam/irasov/filmlibrary/logic/Action.java
+++ src/main/java/com/epam/irasov/filmlibrary/logic/Action.java
@@ -1,9 +1,22 @@
 package com.epam.irasov.filmlibrary.logic;
 
+import com.epam.irasov.filmlibrary.entity.FilmMember;
+import com.epam.irasov.filmlibrary.entity.User;
+
+import java.util.List;
+
 public class Action {
     public static final int PERCENT = 100;
 
-    public static int countRating(int usersNumber,int votersNumber){
+    public static int countRating(int usersNumber, int votersNumber){
         return (votersNumber*PERCENT)/usersNumber;
+    }
+
+    public static int votersNumber(List<FilmMember> filmMembers){
+        return filmMembers.size();
+    }
+
+    public static int usersNumber(List<User> users){
+        return users.size();
     }
 }
