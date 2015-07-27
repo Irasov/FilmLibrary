381970dd8f0dea385ca81b244199228aefa31022
Irasov <srnan@yandex.ru>
2015-07-27 17:52:00 +0600

    delete rating

--- src/main/java/com/epam/irasov/filmlibrary/entity/Review.java
+++ src/main/java/com/epam/irasov/filmlibrary/entity/Review.java
@@ -8,17 +8,15 @@ public class Review extends NamedEntity {
     private LocalDate date;
     private String text;
     private Status status;
-    private Rating rating;
 
     public Review() {
     }
 
-    public Review(Long id, String name, LocalDate date, String text, Status status, Rating rating) {
+    public Review(Long id, String name, LocalDate date, String text, Status status) {
         super(id, name);
         this.date = date;
         this.text = text;
         this.status = status;
-        this.rating = rating;
     }
 
     public LocalDate getDate() {
@@ -45,21 +43,13 @@ public class Review extends NamedEntity {
         this.status = status;
     }
 
-    public Rating getRating() {
-        return rating;
-    }
-
-    public void setRating(Rating rating) {
-        this.rating = rating;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Review)) return false;
         if (!super.equals(o)) return false;
         Review review = (Review) o;
-        return !(date != null ? !date.equals(review.date) : review.date != null) && !(text != null ? !text.equals(review.text) : review.text != null) && status == review.status && !(rating != null ? !rating.equals(review.rating) : review.rating != null);
+        return !(date != null ? !date.equals(review.date) : review.date != null) && !(text != null ? !text.equals(review.text) : review.text != null) && status == review.status;
 
     }
 
@@ -69,7 +59,6 @@ public class Review extends NamedEntity {
         result = 31 * result + (date != null ? date.hashCode() : 0);
         result = 31 * result + (text != null ? text.hashCode() : 0);
         result = 31 * result + (status != null ? status.hashCode() : 0);
-        result = 31 * result + (rating != null ? rating.hashCode() : 0);
         return result;
     }
 }
