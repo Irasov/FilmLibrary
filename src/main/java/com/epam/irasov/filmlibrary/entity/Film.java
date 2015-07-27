3c3560c9f07bc974ce3f54b44e188ad1aafbd93a
Irasov <srnan@yandex.ru>
2015-07-27 21:19:11 +0600

    Rating

--- src/main/java/com/epam/irasov/filmlibrary/entity/Film.java
+++ src/main/java/com/epam/irasov/filmlibrary/entity/Film.java
@@ -17,25 +17,6 @@ public class Film extends NamedEntity {
     private Rating rating;
     private List<Honor> honors;
 
-    public static class Rating extends NamedEntity {
-        private int count;
-        public Rating() {
-        }
-
-        public Rating(Long id, String name, int count) {
-            super(id, name);
-            this.count = count;
-        }
-
-        public int getCount() {
-            return count;
-        }
-
-        public void setCount(int count) {
-            this.count = count;
-        }
-    }
-
     public Film() {
         super();
         members = new ArrayList<>();
