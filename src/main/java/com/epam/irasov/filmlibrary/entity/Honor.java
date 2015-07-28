5c9b425d36a52f8cf8c7c568199deb98052c6461
Irasov <srnan@yandex.ru>
2015-07-28 16:33:04 +0600

    description

--- src/main/java/com/epam/irasov/filmlibrary/entity/Honor.java
+++ src/main/java/com/epam/irasov/filmlibrary/entity/Honor.java
@@ -1,14 +1,17 @@
 package com.epam.irasov.filmlibrary.entity;
 
 public class Honor extends NamedEntity {
-    String image;
+    private String image;
+    private String description;
+
 
     public Honor() {
     }
 
-    public Honor(Long id, String name, String image) {
+    public Honor(Long id, String name, String image, String description) {
         super(id, name);
         this.image = image;
+        this.description = description;
     }
 
     public String getImage() {
@@ -19,20 +22,28 @@ public class Honor extends NamedEntity {
         this.image = image;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Honor)) return false;
         if (!super.equals(o)) return false;
         Honor honor = (Honor) o;
-        return !(image != null ? !image.equals(honor.image) : honor.image != null);
-
+        return !(image != null ? !image.equals(honor.image) : honor.image != null) && !(description != null ? !description.equals(honor.description) : honor.description != null);
     }
 
     @Override
     public int hashCode() {
         int result = super.hashCode();
         result = 31 * result + (image != null ? image.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
         return result;
     }
 }
