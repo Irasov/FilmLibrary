b5c0777ce34bfaf49d93f4c96983f4da865a5d74
Irasov <srnan@yandex.ru>
2015-08-01 12:20:17 +0600

    delete list honors and list genre

--- src/main/java/com/epam/irasov/filmlibrary/entity/SystemMember.java
+++ src/main/java/com/epam/irasov/filmlibrary/entity/SystemMember.java
@@ -1,15 +1,21 @@
 package com.epam.irasov.filmlibrary.entity;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-public abstract class SystemMember extends Member {
+public class SystemMember extends Member {
     private String login;
     private String password;
     private String email;
     private Type type;
+    private List<Review> reviews;
+    private List<Film> films;
+
 
     public SystemMember() {
-
+        reviews = new ArrayList<>();
+        films = new ArrayList<>();
     }
 
     public SystemMember(Long id, String name, String patronymic, String surname, LocalDate birthDate, String photo, String login, String password, String email, Type type) {
@@ -18,6 +24,8 @@ public abstract class SystemMember extends Member {
         this.password = password;
         this.email = email;
         this.type = type;
+        reviews = new ArrayList<>();
+        films = new ArrayList<>();
     }
 
     public String getLogin() {
@@ -52,14 +60,37 @@ public abstract class SystemMember extends Member {
         this.type = type;
     }
 
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public void addReviews(Review review) {
+        reviews.add(review);
+    }
+
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<Film> films) {
+        this.films = films;
+    }
+
+    public void addFilm(Film film) {
+        films.add(film);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof SystemMember)) return false;
         if (!super.equals(o)) return false;
         SystemMember that = (SystemMember) o;
-        return !(login != null ? !login.equals(that.login) : that.login != null) && !(password != null ? !password.equals(that.password) : that.password != null) && !(email != null ? !email.equals(that.email) : that.email != null) && !(type != null ? !type.equals(that.type) : that.type != null);
-
+        return !(login != null ? !login.equals(that.login) : that.login != null) && !(password != null ? !password.equals(that.password) : that.password != null) && !(email != null ? !email.equals(that.email) : that.email != null) && !(type != null ? !type.equals(that.type) : that.type != null) && !(reviews != null ? !reviews.equals(that.reviews) : that.reviews != null) && !(films != null ? !films.equals(that.films) : that.films != null);
     }
 
     @Override
@@ -69,6 +100,8 @@ public abstract class SystemMember extends Member {
         result = 31 * result + (password != null ? password.hashCode() : 0);
         result = 31 * result + (email != null ? email.hashCode() : 0);
         result = 31 * result + (type != null ? type.hashCode() : 0);
+        result = 31 * result + (reviews != null ? reviews.hashCode() : 0);
+        result = 31 * result + (films != null ? films.hashCode() : 0);
         return result;
     }
 }
