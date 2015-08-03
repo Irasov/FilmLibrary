2c89b35b3800c893fd92b1f371b91363f23da10c
Irasov <srnan@yandex.ru>
2015-08-03 12:53:38 +0600

    save

--- src/main/java/com/epam/irasov/filmlibrary/dao/JdbcSystemMemberDao.java
+++ src/main/java/com/epam/irasov/filmlibrary/dao/JdbcSystemMemberDao.java
@@ -1,12 +1,76 @@
 package com.epam.irasov.filmlibrary.dao;
 
+import com.epam.irasov.filmlibrary.entity.Member;
+import com.epam.irasov.filmlibrary.entity.SystemMember;
+
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class JdbcSystemMemberDao implements SystemMemberDao {
+    private final static String SAVE_SYSTEM_MEMBER = "INSERT INTO SYSTEM_MEMBER(NAME, PATRONYMIC, SURNAME, BIRTH_DATE, ID_TYPE, LOGIN, PASSWORD, EMAIL) VALUES(?,?,?,?,?,?,?,?)";
 
     private final Connection connection;
 
     public JdbcSystemMemberDao(Connection connection) {
         this.connection = connection;
+    }
+
+    @Override
+    public SystemMember findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public SystemMember findBySurName(String surName) {
+        return null;
+    }
+
+    @Override
+    public void update(SystemMember systemMember) {
+
+    }
+
+    @Override
+    public SystemMember save(SystemMember systemMember) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SAVE_SYSTEM_MEMBER);
+            setInsertSystemMember(preparedStatement,systemMember.getName(),systemMember.getPatronymic(),systemMember.getSurname(),systemMember.getBirthDate(),systemMember.getType().getId(),systemMember.getLogin(),systemMember.getPassword(),systemMember.getEmail());
+            return systemMember;
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
+    }
+
+    @Override
+    public Member.Type insertMemberType(Member.Type type) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(SystemMember systemMember) {
+        return false;
+    }
+
+    static void setInsertSystemMember(PreparedStatement preparedStatement, String name, String patronymic, String surname, LocalDate birthDate, Long idType, String login, String password, String email) throws SQLException {
+        int index = 1;
+        preparedStatement.setString(index, name);
+        index++;
+        preparedStatement.setString(index, patronymic);
+        index++;
+        preparedStatement.setString(index, surname);
+        index++;
+        preparedStatement.setDate(index, Date.valueOf(birthDate));
+        index++;
+        preparedStatement.setLong(index, idType);
+        index++;
+        preparedStatement.setString(index, login);
+        index++;
+        preparedStatement.setString(index, password);
+        index++;
+        preparedStatement.setString(index, email);
+        preparedStatement.executeUpdate();
     }
 }
--- src/main/java/com/epam/irasov/filmlibrary/dao/SystemMemberDao.java
+++ src/main/java/com/epam/irasov/filmlibrary/dao/SystemMemberDao.java
@@ -1,4 +1,16 @@
 package com.epam.irasov.filmlibrary.dao;
 
+import com.epam.irasov.filmlibrary.entity.Member;
+import com.epam.irasov.filmlibrary.entity.SystemMember;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface SystemMemberDao {
+    SystemMember findById(Long id);
+    SystemMember findBySurName(String surName);
+    void update (SystemMember systemMember);
+    SystemMember save (SystemMember movie);
+    Member.Type insertMemberType(Member.Type type);
+    boolean remove(SystemMember systemMember);
 }
