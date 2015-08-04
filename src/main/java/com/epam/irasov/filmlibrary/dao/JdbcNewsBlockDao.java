553e89bb92e77d137b1fee4e3800cd10b8426c0f
Irasov <srnan@yandex.ru>
2015-08-04 09:56:57 +0600

    news block

--- src/main/java/com/epam/irasov/filmlibrary/dao/JdbcNewsBlockDao.java
+++ src/main/java/com/epam/irasov/filmlibrary/dao/JdbcNewsBlockDao.java
@@ -1,5 +1,8 @@
 package com.epam.irasov.filmlibrary.dao;
 
+import com.epam.irasov.filmlibrary.entity.News;
+import com.epam.irasov.filmlibrary.entity.NewsBlock;
+
 import java.sql.Connection;
 
 public class JdbcNewsBlockDao implements NewsBlockDao{
@@ -7,5 +10,30 @@ public class JdbcNewsBlockDao implements NewsBlockDao{
 
     public JdbcNewsBlockDao(Connection connection) {
         this.connection = connection;
+    }
+
+    @Override
+    public NewsBlock save(NewsBlock newsBlock) {
+        return null;
+    }
+
+    @Override
+    public void addNews(News news) {
+
+    }
+
+    @Override
+    public boolean removeNews(News news) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(NewsBlock newsBlock) {
+        return false;
+    }
+
+    @Override
+    public NewsBlock upDate(NewsBlock newsBlock) {
+        return null;
     }
 }
--- src/main/java/com/epam/irasov/filmlibrary/dao/NewsBlockDao.java
+++ src/main/java/com/epam/irasov/filmlibrary/dao/NewsBlockDao.java
@@ -1,4 +1,14 @@
 package com.epam.irasov.filmlibrary.dao;
 
+import com.epam.irasov.filmlibrary.entity.News;
+import com.epam.irasov.filmlibrary.entity.NewsBlock;
+
+import java.util.List;
+
 public interface NewsBlockDao {
+    NewsBlock save(NewsBlock newsBlock);
+    void addNews (News news);
+    boolean removeNews(News news);
+    boolean remove(NewsBlock newsBlock);
+    NewsBlock upDate(NewsBlock newsBlock);
 }
