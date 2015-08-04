d20d6b06fa7337bf6debabfc8285489bdfd908dc
Irasov <srnan@yandex.ru>
2015-08-04 12:51:49 +0600

    View view

--- src/main/java/com/epam/irasov/filmlibrary/servlet/FrontControllerServlet.java
+++ src/main/java/com/epam/irasov/filmlibrary/servlet/FrontControllerServlet.java
@@ -1,5 +1,9 @@
 package com.epam.irasov.filmlibrary.servlet;
 
+import com.epam.irasov.filmlibrary.action.Action;
+import com.epam.irasov.filmlibrary.action.ActionFactory;
+import com.epam.irasov.filmlibrary.action.View;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,11 +11,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name="FrontControllerServlet", urlPatterns = "/controller")
+@WebServlet(name = "FrontControllerServlet", urlPatterns = "/controller")
 public class FrontControllerServlet extends HttpServlet {
+
+    private ActionFactory factory;
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
+        Action action = factory.getAction(req);
+        if(action==null){
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        View view = action.execute(req,resp);
+        if(view.getRedirect()){
+            resp.sendRedirect(view.getPath() + ".jsp");
+        }else{
+            req.getRequestDispatcher("/"+"index" + ".jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+     public void init() throws ServletException {
+        factory = new ActionFactory();
     }
 }
