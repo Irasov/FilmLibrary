e92209715c50d7b34c07cd4c50f42e23f56e16e4
Irasov <srnan@yandex.ru>
2015-08-07 12:29:57 +0600

    newsBlock

--- src/main/java/com/epam/irasov/filmlibrary/dao/NewsBlockDao.java
+++ src/main/java/com/epam/irasov/filmlibrary/dao/NewsBlockDao.java
@@ -3,13 +3,10 @@ package com.epam.irasov.filmlibrary.dao;
 import com.epam.irasov.filmlibrary.entity.News;
 import com.epam.irasov.filmlibrary.entity.NewsBlock;
 
-import java.util.List;
-
 public interface NewsBlockDao {
     NewsBlock save(NewsBlock newsBlock);
     void addNews (NewsBlock newsBlock, News news);
     boolean removeNews(News news);
-    boolean remove(NewsBlock newsBlock);
     NewsBlock upDate(NewsBlock newsBlock);
     NewsBlock findByIDNewsBlock(Long idNewsBlock);
 }
