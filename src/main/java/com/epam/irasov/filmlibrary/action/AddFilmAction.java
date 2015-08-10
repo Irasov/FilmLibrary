fa0f818516279ff51d75153f2b41d8be46a578d9
Irasov <srnan@yandex.ru>
2015-08-10 20:07:11 +0600

    Add film

---
+++ src/main/java/com/epam/irasov/filmlibrary/action/AddFilmAction.java
@@ -0,0 +1,73 @@
+package com.epam.irasov.filmlibrary.action;
+
+import com.epam.irasov.filmlibrary.dao.DaoException;
+import com.epam.irasov.filmlibrary.dao.DaoFactory;
+import com.epam.irasov.filmlibrary.dao.FilmDao;
+import com.epam.irasov.filmlibrary.entity.Film;
+import com.epam.irasov.filmlibrary.entity.Rating;
+import com.epam.irasov.filmlibrary.validator.Validator;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
+
+public class AddFilmAction implements Action {
+    private final static Long ID_FILM = 1l;
+    private final static Long ID_RATING = 1l;
+    private final static int VOTES_RATING = 0;
+    private final static String PATH_IMAGE = "img/";
+    private final static String MESSAGE = "film.add.complete";
+
+    public AddFilmAction() {
+    }
+
+    @Override
+    public View execute(HttpServletRequest req, HttpServletResponse resp) {
+        String name = req.getParameter("name");
+        String tagLine = req.getParameter("tagLine");
+        String ageRestriction = req.getParameter("age");
+        String duration = req.getParameter("duration");
+        String description = req.getParameter("description");
+        String premiere = req.getParameter("premiere");
+        String cover = req.getParameter("fileName");
+
+        String tagLineError = Validator.isTagLineValid(tagLine);
+        String descriptionError = Validator.isDescriptionValid(description);
+        String restrictionError = Validator.isRestrictionAgeValid(ageRestriction);
+        if (tagLineError != null) {
+            req.setAttribute("fileName", cover);
+            req.setAttribute("tagLineError", tagLineError);
+            return new View("operation-with-movies", false);
+        }
+        if (descriptionError != null) {
+            req.setAttribute("descriptionError", descriptionError);
+            req.setAttribute("fileName", cover);
+            return new View("operation-with-movies", false);
+        }
+        if (restrictionError != null) {
+            req.setAttribute("fileName", cover);
+            req.setAttribute("restrictionError", restrictionError);
+            return new View("operation-with-movies", false);
+        }
+
+        DaoFactory daoFactory = DaoFactory.getInstance();
+        try {
+            Rating rating = daoFactory.newRatingDao().save(new Rating(ID_RATING, name, VOTES_RATING));
+
+            System.out.println(name);
+            daoFactory.beginTx();
+            FilmDao filmDao = daoFactory.newFilmDao();
+            filmDao.save(new Film(ID_FILM, name, tagLine, LocalDate.parse(premiere, ofPattern("yyyy-MM-dd")), Integer.parseInt(ageRestriction), Integer.parseInt(duration), PATH_IMAGE + cover, daoFactory.newRatingDao().findbyName(name), description));
+            req.getSession().setAttribute("message",MESSAGE);
+            daoFactory.endTx();
+        } catch (Exception e) {
+            throw new DaoException(e);
+        } finally {
+            daoFactory.close();
+        }
+
+        return new View("operation-with-movies", true);
+    }
+}
