89c8e64c4beaa96b90d1f1cc1c87fd3bfce9a8c9
Irasov <srnan@yandex.ru>
2015-08-13 13:21:05 +0600

    sorting films

---
+++ src/main/java/com/epam/irasov/filmlibrary/logic/Sorting.java
@@ -0,0 +1,16 @@
+package com.epam.irasov.filmlibrary.logic;
+
+import com.epam.irasov.filmlibrary.entity.Film;
+import com.epam.irasov.filmlibrary.entity.NamedEntity;
+import java.util.Collections;
+import java.util.List;
+
+public class Sorting {
+    public static final String CONST_SORT_NAME = "name";
+
+    public static void sortFilm(List<Film> a, String variant) {
+        if (variant.equals(CONST_SORT_NAME)) {
+            Collections.sort(a, NamedEntity.NAME_ORDER);
+        }
+    }
+}
