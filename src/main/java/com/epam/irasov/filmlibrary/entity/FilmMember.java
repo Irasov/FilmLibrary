eae3c2732a99228302068233f2973fa103a2c50b
Irasov <srnan@yandex.ru>
2015-08-14 20:47:28 +0600

    type

--- src/main/java/com/epam/irasov/filmlibrary/entity/FilmMember.java
+++ src/main/java/com/epam/irasov/filmlibrary/entity/FilmMember.java
@@ -1,32 +1,73 @@
 package com.epam.irasov.filmlibrary.entity;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
-public class FilmMember extends Member {
-    private List<Type> types;
+public abstract class FilmMember extends NamedEntity {
+    private String patronymic;
+    private String surname;
+    private LocalDate birthDate;
+    private String photo;
+    private Type type;
+
+    public static class Type extends NamedEntity {
+        public Type() {
+        }
+
+        public Type(Long id, String name) {
+            super(id, name);
+        }
+    }
 
     public FilmMember() {
-        super();
-        types = new ArrayList<>();
     }
 
-    public FilmMember(Long id, String name, String patronymic, String surname, LocalDate birthDate, String photo) {
-        super(id, name, patronymic, surname, birthDate, photo);
-        types = new ArrayList<>();
+    public FilmMember(Long id, String name, String patronymic, String surname, LocalDate birthDate, String photo, Type type) {
+        super(id, name);
+        this.type = type;
+        this.patronymic = patronymic;
+        this.surname = surname;
+        this.birthDate = birthDate;
+        this.photo = photo;
     }
 
-    public List<Type> getTypes() {
-        return types;
+    public String getPatronymic() {
+        return patronymic;
     }
 
-    public void setTypes(List<Type> types) {
-        this.types = types;
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
     }
 
-    public void addType(Type type){
-        types.add(type);
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
     }
 
     @Override
@@ -34,15 +75,18 @@ public class FilmMember extends Member {
         if (this == o) return true;
         if (!(o instanceof FilmMember)) return false;
         if (!super.equals(o)) return false;
-        FilmMember that = (FilmMember) o;
-        return !(types != null ? !types.equals(that.types) : that.types != null);
-
+        FilmMember member = (FilmMember) o;
+        return !(patronymic != null ? !patronymic.equals(member.patronymic) : member.patronymic != null) && !(surname != null ? !surname.equals(member.surname) : member.surname != null) && !(birthDate != null ? !birthDate.equals(member.birthDate) : member.birthDate != null) && !(photo != null ? !photo.equals(member.photo) : member.photo != null) && !(type != null ? !type.equals(member.type) : member.type != null);
     }
 
     @Override
     public int hashCode() {
         int result = super.hashCode();
-        result = 31 * result + (types != null ? types.hashCode() : 0);
+        result = 31 * result + (patronymic != null ? patronymic.hashCode() : 0);
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + (birthDate != null ? birthDate.hashCode() : 0);
+        result = 31 * result + (photo != null ? photo.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
         return result;
     }
 }
--- src/main/java/com/epam/irasov/filmlibrary/entity/SystemMember.java
+++ src/main/java/com/epam/irasov/filmlibrary/entity/SystemMember.java
@@ -4,11 +4,10 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SystemMember extends Member {
+public class SystemMember extends FilmMember {
     private String login;
     private String password;
     private String email;
-    private Type type;
     private List<Review> reviews;
     private List<Film> films;
 
@@ -19,11 +18,10 @@ public class SystemMember extends Member {
     }
 
     public SystemMember(Long id, String name, String patronymic, String surname, LocalDate birthDate, String photo, String login, String password, String email, Type type) {
-        super(id, name, patronymic, surname, birthDate, photo);
+        super(id, name, patronymic, surname, birthDate, photo,type);
         this.login = login;
         this.password = password;
         this.email = email;
-        this.type = type;
         reviews = new ArrayList<>();
         films = new ArrayList<>();
     }
@@ -50,14 +48,6 @@ public class SystemMember extends Member {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
     }
 
     public List<Review> getReviews() {
@@ -90,7 +80,7 @@ public class SystemMember extends Member {
         if (!(o instanceof SystemMember)) return false;
         if (!super.equals(o)) return false;
         SystemMember that = (SystemMember) o;
-        return !(login != null ? !login.equals(that.login) : that.login != null) && !(password != null ? !password.equals(that.password) : that.password != null) && !(email != null ? !email.equals(that.email) : that.email != null) && !(type != null ? !type.equals(that.type) : that.type != null) && !(reviews != null ? !reviews.equals(that.reviews) : that.reviews != null) && !(films != null ? !films.equals(that.films) : that.films != null);
+        return !(login != null ? !login.equals(that.login) : that.login != null) && !(password != null ? !password.equals(that.password) : that.password != null) && !(email != null ? !email.equals(that.email) : that.email != null) && !(reviews != null ? !reviews.equals(that.reviews) : that.reviews != null) && !(films != null ? !films.equals(that.films) : that.films != null);
     }
 
     @Override
@@ -99,7 +89,6 @@ public class SystemMember extends Member {
         result = 31 * result + (login != null ? login.hashCode() : 0);
         result = 31 * result + (password != null ? password.hashCode() : 0);
         result = 31 * result + (email != null ? email.hashCode() : 0);
-        result = 31 * result + (type != null ? type.hashCode() : 0);
         result = 31 * result + (reviews != null ? reviews.hashCode() : 0);
         result = 31 * result + (films != null ? films.hashCode() : 0);
         return result;
