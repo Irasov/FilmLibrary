b4d6f5afd216eb6414483c397968627b282f3e73
Irasov <srnan@yandex.ru>
2015-08-14 17:53:38 +0600

    delete honor

--- src/main/java/com/epam/irasov/filmlibrary/entity/FilmMember.java
+++ src/main/java/com/epam/irasov/filmlibrary/entity/FilmMember.java
@@ -5,28 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FilmMember extends Member {
-   /* private List<Honor> honors;*/
     private List<Type> types;
 
     public FilmMember() {
         super();
-        /*honors = new ArrayList<>();*/
         types = new ArrayList<>();
     }
 
     public FilmMember(Long id, String name, String patronymic, String surname, LocalDate birthDate, String photo) {
         super(id, name, patronymic, surname, birthDate, photo);
-       /* honors = new ArrayList<>();*/
         types = new ArrayList<>();
     }
-
-   /* public List<Honor> getHonors() {
-        return honors;
-    }
-
-    public void setHonors(List<Honor> honors) {
-        this.honors = honors;
-    }*/
 
     public List<Type> getTypes() {
         return types;
@@ -40,24 +29,19 @@ public class FilmMember extends Member {
         types.add(type);
     }
 
-   /* public void addHonor(Honor honor) {
-        honors.add(honor);
-    }*/
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof FilmMember)) return false;
         if (!super.equals(o)) return false;
         FilmMember that = (FilmMember) o;
-        return /*!(honors != null ? !honors.equals(that.honors) : that.honors != null) && */!(types != null ? !types.equals(that.types) : that.types != null);
+        return !(types != null ? !types.equals(that.types) : that.types != null);
 
     }
 
     @Override
     public int hashCode() {
         int result = super.hashCode();
-       /* result = 31 * result + (honors != null ? honors.hashCode() : 0);*/
         result = 31 * result + (types != null ? types.hashCode() : 0);
         return result;
     }
