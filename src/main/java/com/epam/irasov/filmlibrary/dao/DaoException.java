bd28e26b97ee6ddc60ef1d26965777926eac33c0
Irasov <srnan@yandex.ru>
2015-08-25 14:32:35 +0600

    Logger

--- src/main/java/com/epam/irasov/filmlibrary/dao/DaoException.java
+++ src/main/java/com/epam/irasov/filmlibrary/dao/DaoException.java
@@ -1,18 +1,25 @@
 package com.epam.irasov.filmlibrary.dao;
 
+import org.apache.log4j.Logger;
+
 public class DaoException extends RuntimeException {
+    private static Logger LOGGER = Logger.getLogger(DaoException.class);
+
     public DaoException() {
     }
 
     public DaoException(String message) {
         super(message);
+        LOGGER.info(message);
     }
 
     public DaoException(String message, Throwable cause) {
         super(message, cause);
+        LOGGER.info(message, cause);
     }
 
     public DaoException(Throwable cause) {
         super(cause);
+        LOGGER.info(cause);
     }
 }
